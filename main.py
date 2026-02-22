bfce7ca9f1e645d864f970a5d5b20e4b5b3ebcc5
nurudeen <nurudeendurowade@gamil.com>
2026-02-22 06:18:06 -0500

    Added check computing resource func

--- main.py
+++ main.py
@@ -1,8 +1,28 @@
 import psutil
 
+print("Starting Infra health Monitor ... \n")
 
-cpu = psutil.cpu_percent(interval=1)
 
-print(f"CPU Usage is: {cpu}%")
+output_file = './logs/health.log'
 
-# print("hello world")
+#Get CPU percentage
+
+cpu_usage = psutil.cpu_percent(interval=1)
+
+# Get Disk Usage
+disk_usage = psutil.disk_usage('/').percent
+
+
+#Get Memory Usage
+memory_usage = psutil.virtual_memory().percent
+
+#Save to a log file
+with open (output_file, 'w') as f:
+    print(f"CPU Usage is: {cpu_usage}%", file=f)
+    print(f"Disk Usage is: {disk_usage}%", file=f)
+    print(f"Memory Usage is: {memory_usage}", file=f)
+
+# What is cpu > 80%
+
+if cpu_usage > 80:
+    print("WARNING: High CPU usage detected!")
