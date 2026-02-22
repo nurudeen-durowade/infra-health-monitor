b94788d0fb612061c725ef769972cb7f09246356
nurudeen2432 <nurudeendurowade@gmail.com>
2026-02-22 13:26:13 +0100

    Modified main.py

--- main.py
+++ main.py
@@ -1,29 +1,21 @@
-import psutil
+from monitor import health, alert
 
-print("Starting Infra health Monitor ... \n")
-
-
-output_file = './logs/health.log'
-
-#Get CPU percentage
-
-cpu_usage = psutil.cpu_percent(interval=1)
-
-# Get Disk Usage
-disk_usage = psutil.disk_usage('/').percent
+import time
 
 
-#Get Memory Usage
-memory_usage = psutil.virtual_memory().percent
 
-#Save to a log file
-with open (output_file, 'w') as f:
-    print(f"CPU Usage is: {cpu_usage}%", file=f)
-    print(f"Disk Usage is: {disk_usage}%", file=f)
-    print(f"Memory Usage is: {memory_usage}", file=f)
+while True:
+    cpu = health.check_cpu()
 
-# What if cpu > 80% ?
-# Add a check
+    memory = health.check_memory()
 
-if cpu_usage > 80:
-    print("WARNING: High CPU usage detected!")
+    disk = health.check_disk()
+
+    health.write_to_logs()
+
+    alert.cpu_alert(cpu)
+
+    alert.memory_alert(memory)
+
+    alert.disk_alert(disk)
+
